b4a77ac690207c0c2e690dc35e9493aa7885c24c
Aristotelis Koulousis <aris_pc@hotmail.com>
2023-05-24 12:25:08 +0300

    Constructor and properties for ViewInventoriesByNameUseCase

--- IMS.UseCases/Inventories/ViewInventoriesByNameUseCase.cs
+++ IMS.UseCases/Inventories/ViewInventoriesByNameUseCase.cs
@@ -9,10 +9,17 @@ namespace IMS.UseCases.Inventories
 {
 		public class ViewInventoriesByNameUseCase
 		{
-				public async Task<IEnumerable<Inventory>> ExecuteAsync(string name = "")
-				{
+		private readonly IInventoryRepository inventoryRepository;
 
-				}
+			public ViewInventoriesByNameUseCase(IInventoryRepository inventoryRepository)
+			{
+				this.inventoryRepository = inventoryRepository;
+			}
+
+			public async Task<IEnumerable<Inventory>> ExecuteAsync(string name = "")
+			{
+				return await inventoryRepository.GetInventoriesByNameAsync(name);
+			}
 		}
 }
  
