b6b7950e24058a4340eca20fe50d673ace55f3de
Aristotelis Koulousis <aris_pc@hotmail.com>
2023-05-25 11:29:04 +0300

    Implemented IInventoryRepository interface method by creating a plugin and injected it into program

--- IMS.CoreBusiness/Inventory.cs
+++ IMS.CoreBusiness/Inventory.cs
@@ -1,7 +1,10 @@
 ﻿namespace IMS.CoreBusiness
 {
-		public class Inventory
-		{
-
-		}
+	public class Inventory
+	{
+        public int InventoryId { get; set; }
+		public string InventoryName { get; set; } = string.Empty;
+        public double Quantity { get; set; }
+		public double Price { get; set; }
+    }
 }
